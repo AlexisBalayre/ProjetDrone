cd6157895a776dedc9386e5db2610263b29b32ea
mcc <60859013+AlexisBal@users.noreply.github.com>
2020-12-07 15:58:46 +0100

    master

--- Main.py
+++ Main.py
@@ -2,25 +2,58 @@ import json
 from Classes.Utilisateurs import *
 
 
-# Créations objets Balise 1 et Balise 2
-balise1 = Balise(0, 48.78074845187753, 1.7698891842542932, 5, 10, 0, 0)
-balise2 = Balise(1, 49.78074845187753, 1.8698891842542932, 8, 15, 3, 1)
+# Chargement des données 
+f = open("Data/Utilisateurs.json", "r")
+Json_Utilisateur = f.read()
+if Json_Utilisateur != None : 
+    Dict_Utilisateurs = json.loads(Json_Utilisateur)
+    Donnees = 1
+else:
+    Donnees = 0 # Fichier vide
+f.close()
 
-# Créations objets Mission 1 et Mission 2
-mission1 = Mission(0, '11/10/2020', '15h 31min 00sec', 1, 1)
-mission2 = Mission(1, '13/10/2020', '16h 31min 00sec', 1, 1)
 
-# Créations objets Utilisateur 1 et Utilisateur 2
-utilisateur1 = Utilisateur(1, 'Alexis', 'Balayre', 'Alexis.balayre@gmail.com', 8.78074845187753, 1.7698891842542932, 10, 0, 0, 0, 0, 0, 0, 0)
-utilisateur2 = Utilisateur(0, 'Tom', 'Dupont', 'Tom.dupont@gmail.com', 9.78074845187753, 0.7698891842542932, 16, 0, 1, 1, 1, 1, 1, 1)
-
-# Création objet Utilisateurs
+# Objet Utilisateurs
 utilisateurs = Utilisateurs()
 
-# Ajout des utilisateurs 1 et 2 aux utilisateurs
-utilisateurs.ajouterUtilisateur(utilisateur1.__dict__)
-utilisateurs.ajouterUtilisateur(utilisateur2.__dict__)
 
+# Création des objets Utilisateur 
+if Donnees == 1:
+    for x in Dict_Utilisateurs["utilisateurs"]:
+        utilisateurs.ajouterUtilisateur(Dict_Utilisateurs["utilisateurs"][x])
+        id = x # Id du dernier objet présent dans le dictionnaire
+
+
+# Sauvegarde des modifications 
+def Sauvegarde():
+    utilisateurs_json = json.dumps(utilisateurs.__dict__, indent=4) # Conversion en JSON
+    # Sauvegarde dans le fichier Data/Utilisateurs.json
+    f = open("Data/Utilisateurs.json", "w") 
+    f.write(utilisateurs_json)
+    f.close()
+
+
+# Création d'un utilisateur 
+def CreationUtilisateur(prenom, nom, email, latitude_base, longitude_base, vitesse, mode_camera, suivie_mail, mail_coord, mail_alt, mail_vitesse, mail_batterie, mail_photo):
+    if Donnees == 1:
+        id_utilisateur = int(id) + 1 # Création d'id unique
+    else:
+        id_utilisateur = 0 # Création d'id unique
+    utilisateur = Utilisateur(id_utilisateur, prenom, nom, email, latitude_base, longitude_base, vitesse, mode_camera, suivie_mail, mail_coord, mail_alt, mail_vitesse, mail_batterie, mail_photo)
+    utilisateurs.ajouterUtilisateur(utilisateur.__dict__)
+    Sauvegarde()
+    
+
+# Création d'une mission 
+def CreationMission(jour, heure, planification, mode_photo):
+    pass
+
+
+# Création d'une balise 
+def CreationBalise(latitude, longitude, altitude, vitesse, pause, photo):
+    pass 
+
+'''
 # Ajout des balises aux missions 
 mission1.ajouterBalise(balise1.__dict__)
 mission2.ajouterBalise(balise1.__dict__)
@@ -30,12 +63,9 @@ mission2.ajouterBalise(balise2.__dict__)
 utilisateur1.ajouterMission(mission1.__dict__)
 utilisateur1.ajouterMission(mission2.__dict__)
 utilisateur2.ajouterMission(mission2.__dict__)
+'''
 
-# Sauvegarde 
-utilisateurs_json = json.dumps(utilisateurs.__dict__, indent=4) # convert into JSON
-f = open("Data/Utilisateurs.json", "w")
-f.write(utilisateurs_json)
-f.close()
-
-
+#CreationUtilisateur('NATDU93', 'Dupont', 'Tom.dupont@gmail.com', 9.78074845187753, 0.7698891842542932, 16, 0, 1, 1, 1, 1, 1, 1)
+#CreationMission('11/10/2020', '15h 31min 00sec', 1, 1)
+#CreationBalise(49.78074845187753, 1.8698891842542932, 8, 15, 3, 1)
 
