944d102f5ed8589fc091fd1f32a250266b8e4c1e
mcc <60859013+AlexisBal@users.noreply.github.com>
2021-01-01 23:09:54 +0100

    master

--- Main.py
+++ Main.py
@@ -217,9 +217,35 @@ def ModificationBalise(
     Sauvegarde()
 
 
+# Suppression d'un utilisateur
+def SuppressionUtilisateur(id_utilisateur):
+    utilisateurs.supprimerUtilisateur(id_utilisateur)
+    Sauvegarde()
+
+
+# Suppression d'une mission
+def SuppressionMission(id_utilisateur, id_mission):
+    utilisateur = utilisateurs.getUtilisateur(id_utilisateur)
+    utilisateur.supprimerMission(id_mission)
+    Sauvegarde()
+
+
+# Suppression d'une balise
+def SuppressionBalise(id_utilisateur, id_mission, id_balise):
+    utilisateur = utilisateurs.getUtilisateur(id_utilisateur)
+    mission = utilisateur.getMission(id_mission)
+    mission.supprimerBalise(id_balise)
+    Sauvegarde()
+
+
 # CreationUtilisateur('tom', 'Dupont', 'Tom.dupont@gmail.com', 9.78074845187753, 0.7698891842542932, 16, 0, 1, 1, 1, 1, 1, 1)
 # CreationMission(0, '13/10/2020', '15h 31min 00sec', 1, 1)
 # CreationBalise(0, 1, 53.78074845187753, 1.8698891842542932, 8, 15, 3, 1)
+
 # ModificationUtilisateur(1, 'Alexis', 'Dupont', 'Tom.dupont@gmail.com', 9.78074845187753, 0.7698891842542932, 16, 0, 1, 1, 1, 0, 0, 0)
 # ModificationMission(0, 1, '15/04/2021', '16h 31min 00sec', 0, 0)
 # ModificationBalise(0, 1, 2, 58.78074845187753, 2.8698891842542932, 20, 30, 4, 0)
+
+# SuppressionUtilisateur(2)
+# SuppressionMission(0, 0)
+# SuppressionBalise(0, 1, 1)
