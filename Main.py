ebe476f71c88d3dac372380a99a45f54195426a5
mcc <60859013+AlexisBal@users.noreply.github.com>
2021-01-01 22:53:54 +0100

    master

--- Main.py
+++ Main.py
@@ -2,12 +2,23 @@ import json
 from Classes.Utilisateurs import *
 
 
-# Chargement des données 
+# Sauvegarde des modifications
+def Sauvegarde():
+    utilisateurs_json = json.dumps(
+        utilisateurs, default=lambda o: o.__dict__, indent=4
+    )  # Conversion en JSON
+    # Sauvegarde dans le fichier Data/Utilisateurs.json
+    f = open("Data/Utilisateurs.json", "w")
+    f.write(utilisateurs_json)
+    f.close()
+
+
+# Chargement des données
 f = open("Data/Utilisateurs.json", "r")
 Json_Utilisateur = f.read()
-Dict_Utilisateurs = json.loads(Json_Utilisateur) 
-if Dict_Utilisateurs == {} : 
-    Donnees = 0 # Fichier vide
+Dict_Utilisateurs = json.loads(Json_Utilisateur)
+if Dict_Utilisateurs == {}:
+    Donnees = 0  # Fichier vide
 else:
     Donnees = 1
 f.close()
@@ -17,46 +28,95 @@ f.close()
 utilisateurs = Utilisateurs()
 
 
-# Création des objets Utilisateur 
+# Création des objets Utilisateur
 if Donnees == 1:
     for x in Dict_Utilisateurs["utilisateurs"]:
         u = Dict_Utilisateurs["utilisateurs"][x]
-        utilisateur = Utilisateur(u["id_utilisateur"], u["prenom"], u["nom"], u["email"], u["latitude_base"], u["longitude_base"], u["vitesse"], u["mode_camera"], u["suivie_mail"], u["mail_coord"], u["mail_alt"], u["mail_vitesse"], u["mail_batterie"], u["mail_photo"])
-        # Création des objets Mission 
+        utilisateur = Utilisateur(
+            u["id_utilisateur"],
+            u["prenom"],
+            u["nom"],
+            u["email"],
+            u["latitude_base"],
+            u["longitude_base"],
+            u["vitesse"],
+            u["mode_camera"],
+            u["suivie_mail"],
+            u["mail_coord"],
+            u["mail_alt"],
+            u["mail_vitesse"],
+            u["mail_batterie"],
+            u["mail_photo"],
+        )
+        # Création des objets Mission
         for y in u["missions"]:
             v = u["missions"][y]
-            mission = Mission(v["id_mission"], v["jour"], v["heure"], v["planification"], v["mode_photo"])
+            mission = Mission(
+                v["id_mission"],
+                v["jour"],
+                v["heure"],
+                v["planification"],
+                v["mode_photo"],
+            )
             # Création des objets Balise
             for z in v["balises"]:
                 w = v["balises"][z]
-                balise = Balise(w["id_balise"], w["latitude"], w["longitude"], w["altitude"], w["vitesse"], w["pause"], w["photo"])
+                balise = Balise(
+                    w["id_balise"],
+                    w["latitude"],
+                    w["longitude"],
+                    w["altitude"],
+                    w["vitesse"],
+                    w["pause"],
+                    w["photo"],
+                )
                 mission.ajouterBalise(balise)
             utilisateur.ajouterMission(mission)
         utilisateurs.ajouterUtilisateur(utilisateur)
-        id = x # Id du dernier objet présent dans le dictionnaire
+        id = x  # Id du dernier objet présent dans le dictionnaire
 
 
-# Sauvegarde des modifications 
-def Sauvegarde():
-    utilisateurs_json = json.dumps(utilisateurs, default=lambda o: o.__dict__, indent=4) # Conversion en JSON
-    # Sauvegarde dans le fichier Data/Utilisateurs.json
-    f = open("Data/Utilisateurs.json", "w") 
-    f.write(utilisateurs_json)
-    f.close()
-
-
-# Création d'un utilisateur 
-def CreationUtilisateur(prenom, nom, email, latitude_base, longitude_base, vitesse, mode_camera, suivie_mail, mail_coord, mail_alt, mail_vitesse, mail_batterie, mail_photo):
+# Création d'un utilisateur
+def CreationUtilisateur(
+    prenom,
+    nom,
+    email,
+    latitude_base,
+    longitude_base,
+    vitesse,
+    mode_camera,
+    suivie_mail,
+    mail_coord,
+    mail_alt,
+    mail_vitesse,
+    mail_batterie,
+    mail_photo,
+):
     if Donnees == 1:
-        id_utilisateur = int(id) + 1 # Création d'id unique
+        id_utilisateur = int(id) + 1  # Création d'id unique
     else:
-        id_utilisateur = 0 # Création d'id unique
-    utilisateur = Utilisateur(id_utilisateur, prenom, nom, email, latitude_base, longitude_base, vitesse, mode_camera, suivie_mail, mail_coord, mail_alt, mail_vitesse, mail_batterie, mail_photo)
+        id_utilisateur = 0  # Création d'id unique
+    utilisateur = Utilisateur(
+        id_utilisateur,
+        prenom,
+        nom,
+        email,
+        latitude_base,
+        longitude_base,
+        vitesse,
+        mode_camera,
+        suivie_mail,
+        mail_coord,
+        mail_alt,
+        mail_vitesse,
+        mail_batterie,
+        mail_photo,
+    )
     utilisateurs.ajouterUtilisateur(utilisateur)
     Sauvegarde()
-    
-    
-# Création d'une mission 
+
+
+# Création d'une mission
 def CreationMission(id_utilisateur, jour, heure, planification, mode_photo):
     utilisateur = utilisateurs.getUtilisateur(id_utilisateur)
     if utilisateur.__dict__["missions"] == {}:
@@ -64,14 +124,16 @@ def CreationMission(id_utilisateur, jour, heure, planification, mode_photo):
     else:
         for x in utilisateur.missions:
             id = x
-        id_mission = int(id) + 1 
+        id_mission = int(id) + 1
     mission = Mission(id_mission, jour, heure, planification, mode_photo)
     utilisateur.ajouterMission(mission)
     Sauvegarde()
-        
 
-# Création d'une balise 
-def CreationBalise(id_utilisateur, id_mission, latitude, longitude, altitude, vitesse, pause, photo):
+
+# Création d'une balise
+def CreationBalise(
+    id_utilisateur, id_mission, latitude, longitude, altitude, vitesse, pause, photo
+):
     utilisateur = utilisateurs.getUtilisateur(id_utilisateur)
     mission = utilisateur.getMission(id_mission)
     if mission.__dict__["balises"] == {}:
@@ -79,14 +141,85 @@ def CreationBalise(id_utilisateur, id_mission, latitude, longitude, altitude, vi
     else:
         for x in mission.balises:
             id = x
-        id_balise = int(id) + 1 
+        id_balise = int(id) + 1
     balise = Balise(id_balise, latitude, longitude, altitude, vitesse, pause, photo)
     mission.ajouterBalise(balise)
     Sauvegarde()
 
 
+# Modification d'un utilisateur
+def ModificationUtilisateur(
+    id_utilisateur,
+    prenom,
+    nom,
+    email,
+    latitude_base,
+    longitude_base,
+    vitesse,
+    mode_camera,
+    suivie_mail,
+    mail_coord,
+    mail_alt,
+    mail_vitesse,
+    mail_batterie,
+    mail_photo,
+):
+    utilisateur = utilisateurs.getUtilisateur(id_utilisateur)
+    utilisateur.setPrenom(prenom)
+    utilisateur.setEmail(email)
+    utilisateur.setVitesse(vitesse)
+    utilisateur.setLatitudeBase(latitude_base)
+    utilisateur.setLongitudeBase(longitude_base)
+    utilisateur.setModeCamera(mode_camera)
+    utilisateur.setSuivieMail(suivie_mail)
+    utilisateur.setMailCoord(mail_coord)
+    utilisateur.setMailAlt(mail_alt)
+    utilisateur.setMailVitesse(mail_vitesse)
+    utilisateur.setMailBatterie(mail_batterie)
+    utilisateur.setMailPhoto(mail_photo)
+    Sauvegarde()
 
-#CreationUtilisateur('tom', 'Dupont', 'Tom.dupont@gmail.com', 9.78074845187753, 0.7698891842542932, 16, 0, 1, 1, 1, 1, 1, 1)
-#CreationMission(0, '13/10/2020', '15h 31min 00sec', 1, 1)
-#CreationBalise(0, 1, 53.78074845187753, 1.8698891842542932, 8, 15, 3, 1)
 
+# Modification d'une mission
+def ModificationMission(
+    id_utilisateur, id_mission, jour, heure, planification, mode_photo
+):
+    utilisateur = utilisateurs.getUtilisateur(id_utilisateur)
+    mission = utilisateur.getMission(id_mission)
+    mission.setJour(jour)
+    mission.setHeure(heure)
+    mission.setPlanification(planification)
+    mission.setModePhoto(mode_photo)
+    Sauvegarde()
+
+
+# Modification d'une balise
+def ModificationBalise(
+    id_utilisateur,
+    id_mission,
+    id_balise,
+    latitude,
+    longitude,
+    altitude,
+    vitesse,
+    pause,
+    photo,
+):
+    utilisateur = utilisateurs.getUtilisateur(id_utilisateur)
+    mission = utilisateur.getMission(id_mission)
+    balise = mission.getBalise(id_balise)
+    balise.setLatitude(latitude)
+    balise.setLongitude(longitude)
+    balise.setAltitude(altitude)
+    balise.setVitesse(vitesse)
+    balise.setPause(pause)
+    balise.setPhoto(photo)
+    Sauvegarde()
+
+
+# CreationUtilisateur('tom', 'Dupont', 'Tom.dupont@gmail.com', 9.78074845187753, 0.7698891842542932, 16, 0, 1, 1, 1, 1, 1, 1)
+# CreationMission(0, '13/10/2020', '15h 31min 00sec', 1, 1)
+# CreationBalise(0, 1, 53.78074845187753, 1.8698891842542932, 8, 15, 3, 1)
+# ModificationUtilisateur(1, 'Alexis', 'Dupont', 'Tom.dupont@gmail.com', 9.78074845187753, 0.7698891842542932, 16, 0, 1, 1, 1, 0, 0, 0)
+# ModificationMission(0, 1, '15/04/2021', '16h 31min 00sec', 0, 0)
+# ModificationBalise(0, 1, 2, 58.78074845187753, 2.8698891842542932, 20, 30, 4, 0)
